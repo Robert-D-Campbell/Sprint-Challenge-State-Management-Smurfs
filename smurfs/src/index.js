2b8953302e3441a9ea0de72025bfca999f822983
Robert Campbell <robertc1191@Roberts-MacBook-Pro-2.local>
2019-11-08 08:40:10 -0800

    folder structure set up, dependencies, index.js imports

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "reac-redux";
+import thunk from "redux-thunk";
+
 import "./index.css";
+
+import { rootReducer } from "./reducers/rootReducer";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
